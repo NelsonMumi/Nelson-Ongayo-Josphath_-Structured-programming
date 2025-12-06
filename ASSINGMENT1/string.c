9e3b7d60f54be91295f934217b56ec8046770d36
NelsonMumi <josphathongayonelson@gmail.com>
2025-12-06 19:23:22 +0300

    Add files via upload

--- ASSINGMENT1/string.c
+++ ASSINGMENT1/string.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name1[100], name2[100];
+    int totalLength;
 
+    printf("Enter your FIRST NAME:\n ");
+    scanf("%s", name1);
+
+    printf("Enter your LAST NAME:\n ");
+    scanf("%s", name2);
+
+    totalLength = strlen(name1) + strlen(name2);
+
+    printf("Hello, %s %s, Your name has %d characters.\n", name1, name2, totalLength);
+
+    return 0;
+}
